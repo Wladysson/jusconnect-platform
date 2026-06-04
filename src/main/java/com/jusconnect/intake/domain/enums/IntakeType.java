e78562dba3676ee2fa2d106a835d19ee8da4eaa5
wladysson <wladysonaraujo991@gmail.com>
2026-06-03 23:09:14 -0300

    feat:  Adiciona intake type enum

---
+++ src/main/java/com/jusconnect/intake/domain/enums/IntakeType.java
@@ -0,0 +1,16 @@
+package com.jusconnect.intake.domain.enums;
+
+public enum IntakeType {
+
+    CIVIL,
+    LABOR,
+    CRIMINAL,
+    FAMILY,
+    CONSUMER,
+    TAX,
+    CORPORATE,
+    IMMIGRATION,
+    REAL_ESTATE,
+    OTHER
+
+}
