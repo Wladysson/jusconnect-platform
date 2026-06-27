f0cf8b01355b88d550df3d159d980d687d3c24e3
wladysson <wladysonaraujo991@gmail.com>
2026-06-27 07:49:38 -0300

    feat(scheduling-domain): adiciona enum de status dos agendamentos, adiciona enum de tipos de agendamento

---
+++ src/main/java/com/jusconnect/scheduling/domain/enums/AppointmentStatus.java
@@ -0,0 +1,18 @@
+package com.jusconnect.scheduling.domain.enums;
+
+public enum AppointmentStatus {
+
+    PENDING,
+
+    CONFIRMED,
+
+    IN_PROGRESS,
+
+    COMPLETED,
+
+    RESCHEDULED,
+
+    CANCELLED,
+
+    NO_SHOW
+}
---
+++ src/main/java/com/jusconnect/scheduling/domain/enums/AppointmentType.java
@@ -0,0 +1,10 @@
+package com.jusconnect.scheduling.domain.enums;
+
+public enum AppointmentType {
+
+    ONLINE,
+
+    IN_PERSON,
+
+    PHONE_CALL
+}
